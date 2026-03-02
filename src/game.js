2e6e86ea32e0fdab8e5e0b5ec972fad789cde748
Supersonic Seven <b.rockholz@icloud.com>
2026-03-02 14:14:33 -0500

    Update game.js
    Yay

--- src/game.js
+++ src/game.js
@@ -1218,7 +1218,7 @@ export function getTTPerSecond() {
 export function gainedCelestialPoints() {
   if (!player.break2) return DC.D1;
   let cp = player.celestials.pelle.records.totalEndgameAntimatter.add(1).log10().div(9e15);
-  if (Achievement(197).isUnlocked && !player.disablePostReality) {
+  if (Achievement(207).isUnlocked && !player.disablePostReality) {
     cp = cp.times(Decimal.max(9e115, player.celestials.pelle.records.totalEndgameAntimatter.add(1).log10()).div(9e115));
   }
   cp = Alpha.isDestroyed ? cp : Decimal.min(cp, DC.NUMMAX.sub(player.endgame.celestialPoints));
