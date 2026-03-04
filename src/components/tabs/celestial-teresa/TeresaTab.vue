b5d07f8dd74d8734870304b4c2eef9bb9676e0ac
Supersonic Seven <b.rockholz@icloud.com>
2026-03-04 15:58:12 -0500

    Update TeresaTab.vue
    Strings

--- src/components/tabs/celestial-teresa/TeresaTab.vue
+++ src/components/tabs/celestial-teresa/TeresaTab.vue
@@ -96,9 +96,11 @@ export default {
       return GameDatabase.celestials.descriptions[0].effects();
     },
     lastMachinesString() {
-      return this.lastMachines.lt(DC.E10000)
-        ? `${quantify("Reality Machine", this.lastMachines, 2)}`
-        : `${quantify("Imaginary Machine", this.lastMachines.dividedBy(DC.E10000), 2)}`;
+      return this.lastMachines.lt(DC.E20000)
+        ? `${quantify("Dual Machine", this.lastMachines.dividedBy(DC.E20000), 2)}`
+        : (this.lastMachines.lt(DC.E10000)
+           ? `${quantify("Reality Machine", this.lastMachines, 2)}`
+           : `${quantify("Imaginary Machine", this.lastMachines.dividedBy(DC.E10000), 2)}`);
     },
     unlockInfoTooltipArrowStyle() {
       return {
